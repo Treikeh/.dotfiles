9b4ee31fbe6c3d0e3fb84a17f485d788e8592b13
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-03-08 19:07:54 +0100

    Removed the protonvpn-cli package since it didn't work

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -70,7 +70,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
-    pkgs-unstable.proton-vpn-cli
   ];
 
   environment = {
