8376ee47c4835276bf05bfec5831bfe2600dfcea
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-03-10 18:04:47 +0100

    Added btop and yazi to the server config

--- nixos/server/configuration.nix
+++ nixos/server/configuration.nix
@@ -70,6 +70,10 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+
+    btop
+    yazi
+
     jellyfin
     jellyfin-web
     jellyfin-ffmpeg
